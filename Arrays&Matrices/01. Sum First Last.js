226e2dba739881e208e401d93fab90324508e5b9
Stoyan24 <stoiansmilqnov24@gmail.com>
2017-05-31 20:29:21 +0300

    First 3 problems solved

---
+++ Arrays&Matrices/01. Sum First Last.js
@@ -0,0 +1,4 @@
+function sumFirstLast(arr) {
+    return Number(arr[0])+Number(arr[arr.length-1]);
+}
+console.log()
---
+++ Arrays&Matrices/02. Even Position Elements.js
@@ -0,0 +1,7 @@
+function evenPositions(arr) {
+    let result = [];
+    for (let i in arr)
+        if (i % 2 == 0)
+            result.push(arr[i]);
+    return result.join(' ');
+}
