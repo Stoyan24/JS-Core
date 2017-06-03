a6508ee89956fc1123df9eeedcd3c8bfe89c2e97
Stoyan24 <stoiansmilqnov24@gmail.com>
2017-06-03 18:41:10 +0300

    Got the fucking shit task

--- ArraysandMatricesExercises/07. Magic Matrices.js
+++ ArraysandMatricesExercises/07. Magic Matrices.js
@@ -1,30 +1,21 @@
-function equal(matrix) {
+function trueOrFalseMatrix(matrix) {
 
-    let result = 0;
-    let sum = 0;
-    for (let row = 0; row < matrix.length; row++) {
-        sum = 0;
-        for (let col = 0; col < matrix[row].length; col++) {
-            sum += matrix[row][col];
+    let result1 = 0;
+    let result2 = 0;
+    for(let row = 0; row < 1; row++) {
+        for (let col = 0; col < 1; col++) {
+            result1 += matrix[row][col];
+            result2 += matrix[col][row];
         }
+
     }
-    console.log(sum);
-    let sum2 = 0;
-    for (let col = 0; col < matrix.length; col++) {
-        sum2 = 0;
-        for (let row = 0; row < matrix[col].length; row++) {
-            sum2 += matrix[row][col];
-        }
-    }
-    console.log(sum2);
-    if(sum == sum2){
+    if(result1 === result2){
         console.log('true');
-    }else {
-        console.log('false');
+    }else{
+        console.log('false')
     }
-
 }
-console.log(equal(
-    [[4, 5, 6],
-    [6, 5, 4],
-    [5, 5, 5]]));
+console.log(trueOrFalseMatrix(
+    [[1, 0, 0],
+        [0, 0, 1],
+        [0, 1, 0]]));
