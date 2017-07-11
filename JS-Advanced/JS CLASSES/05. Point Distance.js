8adb2297226007499144523b7bd8a92d0f6cdc92
Stoyan24 <stoiansmilqnov24@gmail.com>
2017-07-11 21:08:03 +0300

    fuck i have so little time this days

---
+++ JS-Advanced/JS CLASSES/05. Point Distance.js
@@ -0,0 +1,17 @@
+class Point {
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+    }
+
+    static distance(a,b){
+        const dx = a.x - b.x;
+        const dy = a.y - b.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+}
+let p1 = new Point(1,3);
+let p2 = new Point(5,-1);
+
+console.log(p1);
+console.log(p2);
